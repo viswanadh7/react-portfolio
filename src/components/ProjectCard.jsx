0b7b2bfd953bba0fba58314176999648d7d236d5
viswanadh7 <mudunuruviswanadh@gmail.com>
2024-10-14 09:52:51 +0530

    Updated resume and experience and resolved bugs

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,13 +7,13 @@ function ProjectCard({ url, name, link }) {
             <motion.div initial={{ scale: 0.5 }} whileInView={{ scale: 1 }} style={{ backgroundSize: "cover", backgroundImage: `url(${url})` }} className='h-80 w-full text-center hover:scale-105 duration-300 hover:shadow-lg hover:shadow-black rounded-2xl md:hidden'>
                 <div className='bg-gradient-to-t from-black to-transparent hover:bg-gradient-to-t hover:from-black hover:to-transparent h-full pt-52 rounded-2xl'>
                     <h1 className='text-2xl text-white mb-5'>{name}</h1>
-                    <a className='border border-white rounded-lg px-5 py-2 text-white hover:px-7 duration-300' href={link} target='_blank'>Live Demo</a>
+                    <a className='border border-white rounded-lg px-5 py-2 text-white hover:px-7 duration-300' href={link} target='_blank' rel="noreferrer">Live Demo</a>
                 </div>
             </motion.div>
             <div style={{ backgroundSize: "cover", backgroundImage: `url(${url})` }} className='h-80 w-full text-center hover:scale-105 duration-300 hover:shadow-lg hover:shadow-black rounded-2xl hidden md:block'>
                 <div className='bg-gradient-to-t from-black to-transparent hover:bg-gradient-to-t hover:from-black hover:to-transparent h-full pt-52 rounded-2xl'>
                     <h1 className='text-2xl text-white mb-5'>{name}</h1>
-                    <a className='border border-white rounded-lg px-5 py-2 text-white hover:px-7 duration-300' href={link} target="_blank">Live Demo</a>
+                    <a className='border border-white rounded-lg px-5 py-2 text-white hover:px-7 duration-300' href={link} target="_blank" rel="noreferrer">Live Demo</a>
 
                 </div>
             </div>
